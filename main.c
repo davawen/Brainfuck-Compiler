71ebf651ed963ebac569346ef55103830a92fcb4
davawen <arthaudawen.26@gmail.com>
2021-08-27 23:34:48 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main(int argc, char *argv[])
+{
+	if(argc <= 1)
+	{
+		printf("Error: no file specified.");
+		exit(-1);
+	}
+	
+	FILE *fp = fopen(argv[1], "r");
+
+	if(fp == NULL)
+	{
+		printf("Error: Couldn't open file.\n");
+		exit(-1);
+	}
+	
+	char memory[30000];
+	
+	char *ptr = memory;
+	
+	int layer = 0;
+	
+	char chr;
+	while((chr = fgetc(fp)) != EOF)
+	{
+		switch(chr)
+		{
+			case '>':
+				ptr++;
+				break;
+			case '<':
+				ptr--;
+				break;
+			case '+':
+				(*ptr)++;
+				break;
+			case '-':
+				(*ptr)--;
+				break;
+			case '.':
+				putchar(*ptr);
+				break;
+			case ',':
+				*ptr = getchar();
+				break;
+			case '[':
+				// lastBracket = ftell(fp);
+				
+				if(*ptr != 0) continue;
+				
+				while((chr = fgetc(fp)) != EOF)
+				{
+					if(chr == '[') layer++;
+					else if(chr == ']')
+					{
+						if(layer == 0) break;
+						else layer--;
+					}
+				}
+				break;
+			case ']':
+				if(*ptr == 0) continue;
+				
+				for(;;)
+				{
+					fseek(fp, -2L, SEEK_CUR);
+					if(ftell(fp) < 0)
+					{
+						printf("Error: no opening bracket specified");
+						exit(-1);
+					}
+					
+					chr = fgetc(fp);
+					
+					if(chr == ']') layer++;
+					else if(chr == '[')
+					{
+						if(layer == 0) break;
+						else layer--;
+					}
+				}
+				
+				// if(lastBracket == -1)
+				// {
+				// }
+				// else
+				// {
+				// 	fseek(fp, lastBracket, SEEK_SET);
+				// }
+				break;
+			default:
+				break;
+		}
+	}
+	
+	fclose(fp);
+	
+	printf("\n"); // Padding
+	
+	return 0;
+}
